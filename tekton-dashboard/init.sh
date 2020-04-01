ed954cc2c05771cbf9e0fa5f47c8649e5aa7c1f2
Alan Greene <ALANGREE@ie.ibm.com>
2020-04-01 23:03:57 +0100

    Cleanup init script

--- tekton-dashboard/init.sh
+++ tekton-dashboard/init.sh
@@ -2,24 +2,7 @@
 sleep 1
 
 # Start Kubernetes
-#launch.sh
-
 minikube start && minikube addons enable ingress
 
-#apt update
-
-#apt install -y kubeadm=1.15.11-00 kubectl=1.15.11-00 kubelet=1.15.11-00
-
-# kubeadm reset --force
-
-#kubeadm init --kubernetes-version $(kubeadm version -o short)
-
-# # Install Tekton Pipelines
-# kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-
-# # Install Tekton Triggers
-# kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
-
-# # Install Tekton Dashboard
-# kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.3.0/dashboard-latest-release.yaml
-# kubectl port-forward -n tekton-pipelines svc/tekton-dashboard 9097:9097 &
+# Wait
+launch.sh
